c23f394a9dff0bcb663f2fcee11e87d74c796544
Victor Carceler <vcarceler@elpuig.xeill.net>
2024-04-29 19:38:26 +0200

    /collect muestra IP del cliente.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //
 // rust-sirin <IP> <PORT> <SECRET>
 
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 
 #[get("/")]
@@ -13,6 +13,14 @@ async fn root() -> impl Responder {
 async fn launcher(req_body: String) -> impl Responder {
 
     HttpResponse::Ok().body(req_body)
+}
+
+#[get("/collect")]
+pub async fn collect(req: HttpRequest) -> impl Responder {
+    if let Some(val) = req.peer_addr() {
+        println!("Address {:?}", val.ip());
+    };
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
@@ -27,6 +35,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(root)
             .service(launcher)
+            .service(collect)
     })
     .workers(1) // Solo queremos 1 hilo
     .bind((ip, port.parse::<u16>().unwrap()))?
